f12ba4ed65f8dbdfe6fac62b5b1fba7553ab2ec1
turist25 <38704426+turist25@users.noreply.github.com>
2018-05-05 21:04:19 +0300

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -1,12 +1,12 @@
 function rozrah() {
 var k = 0;
-if ( document.forms[0].r2.checked == true) {
+if ( document.forms[0].r0.checked == true) {
 k = k + 1; }
-if ( document.forms[1].r5.checked == true) {
+if ( document.forms[1].r6.checked == true) {
 k = k + 1; }
-if ( document.forms[2].r9.checked == true) {
+if ( document.forms[2].r11.checked == true) {
 k = k + 1; }
-if ( document.forms[3].r15.checked == true) {
+if ( document.forms[3].r16.checked == true) {
 k = k + 1; }
 if ( document.forms[4].r20.checked == true) {
 k = k + 1; }
